c9b58d97f3d0f6debeb6c4631e1bf543bc7acd3b
Haohsiang <haohsiang0509@gmail.com>
2017-02-18 15:36:39 +0800

    0218

---
+++ avian_influenza_map2/display.js
@@ -0,0 +1,79 @@
+function displayPic1(){
+	if(document.getElementById("pic1").style.display=="none"){
+		document.getElementById("pic1").style.display="";
+	}else{
+		document.getElementById("pic1").style.display="none";
+	}
+}
+
+function displayPic2(){
+	if(document.getElementById("pic2").style.display=="none"){
+		document.getElementById("pic2").style.display="";
+	}else{
+		document.getElementById("pic2").style.display="none";
+	}
+}
+
+function displayPic3(){
+	if(document.getElementById("pic3").style.display=="none"){
+		document.getElementById("pic3").style.display="";
+	}else{
+		document.getElementById("pic3").style.display="none";
+	}
+}
+
+function displayPic4(){
+	if(document.getElementById("pic4").style.display=="none"){
+		document.getElementById("pic4").style.display="";
+	}else{
+		document.getElementById("pic4").style.display="none";
+	}
+}
+
+function displayPic5(){
+	if(document.getElementById("pic5").style.display=="none"){
+		document.getElementById("pic5").style.display="";
+	}else{
+		document.getElementById("pic5").style.display="none";
+	}
+}
+
+function displayPic6(){
+	if(document.getElementById("pic6").style.display=="none"){
+		document.getElementById("pic6").style.display="";
+	}else{
+		document.getElementById("pic6").style.display="none";
+	}
+}
+
+function displayPic7(){
+	if(document.getElementById("pic7").style.display=="none"){
+		document.getElementById("pic7").style.display="";
+	}else{
+		document.getElementById("pic7").style.display="none";
+	}
+}
+
+function displayPic8(){
+	if(document.getElementById("pic8").style.display=="none"){
+		document.getElementById("pic8").style.display="";
+	}else{
+		document.getElementById("pic8").style.display="none";
+	}
+}
+
+function displayPic9(){
+	if(document.getElementById("pic9").style.display=="none"){
+		document.getElementById("pic9").style.display="";
+	}else{
+		document.getElementById("pic9").style.display="none";
+	}
+}
+
+function displayPic10(){
+	if(document.getElementById("pic10").style.display=="none"){
+		document.getElementById("pic10").style.display="";
+	}else{
+		document.getElementById("pic10").style.display="none";
+	}
+}
